1495b93707477efb4937daf0e9a9afabad835b33
mohamed shahid s <mohamedshahids@gmail.com>
2018-02-28 05:07:34 +0000

    first commit

---
+++ src/main/java/com/shahid/app/App.java
@@ -0,0 +1,13 @@
+package com.shahid.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is sample java!" );
+    }
+}
